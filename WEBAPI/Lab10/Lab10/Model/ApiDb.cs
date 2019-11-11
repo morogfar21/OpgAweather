13061f64e0a3aa48f66f950ff10902b1c7d613e3
martinsekd <201710685@post.au.dk>
2019-11-11 10:37:56 +0100

    Mongo

---
+++ WEBAPI/Lab10/Lab10/Model/ApiDb.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab10.Model
+{
+    public class ApiDb : DbContext
+    {
+        public ApiDb()
+        {
+        }
+
+        public ApiDb(DbContextOptions<ApiDb> options)
+        :base(options)
+        {
+
+        }
+
+        public virtual DbSet<measurement>
+
+    }
+}
